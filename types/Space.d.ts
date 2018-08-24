623f977315c98780a78113df690d8abb50aa5e89
Joakim Gunst <joakim.gunst@rapal.fi>
2018-08-24 15:16:48 +0300

    Updated all dependencies.

--- types/Space.d.ts
+++ types/Space.d.ts
@@ -5,7 +5,7 @@ export declare class Space extends Element {
      * A space is an element that is created based on vector boundaries.
      */
     constructor(id: string, boundaries: Boundary[], polylineOptions?: L.PolylineOptions, elementOptions?: Partial<ElementOptions>);
-    private static getPolygons(boundaries, polylineOptions?);
+    private static getPolygons;
 }
 export interface Boundary {
     isVoid: boolean;
